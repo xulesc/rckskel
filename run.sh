461e1b5f5e0d087ada1977e32862289d3e6eab68
Anuj Sharma <anuj.sharma80@gmail.com>
2015-05-05 15:27:32 +0300

    disable run

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 ##
-./scripts/build.sh
+#./scripts/build.sh
 ##
 #valgrind -v --leak-check=full --show-reachable=yes --trace-children=yes 
 #./RCCE_V1.0.13/rccerun  -nue $1 -f RCCE_V1.0.13/hosts/rc.hosts $PWD/test/test
